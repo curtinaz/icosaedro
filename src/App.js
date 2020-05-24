c78f8322934ded3e34bce9430d63e72427973e50
curtinaz <curtinazif@gmail.com>
2020-05-24 15:20:02 -0300

    added some animations

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Loading from './components/loading';
+import Container from './components/container';
 import Header from './components/header';
 
 const App = () => (
   <div className="App">
     <Loading />
+    <Container />
     <Header />
   </div>
 );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => (
-    <header id="main-header">Icosaedro</header>
+    <header id="main-header">
+        <div>
+            <h1>Entrar em uma sala</h1>
+        </div>
+    </header>
 );
 
 export default Header;
